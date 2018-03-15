f887772b9dfa4495c096a062fecae1078b62842c
Max Aigner <max.wolfgang.aigner@gmail.com>
2018-03-15 17:25:48 +0100

    added Photon Tutorial Data

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Com.MyCompany.MyGame
+{
+    public class GameManager : Photon.PunBehaviour
+    {
+        public static GameManager Instance;
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+        private void Start()
+        {
+            Instance = this;
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            } else
+            {
+                Debug.Log("We are Instantiating LocalPlayer from " + SceneManager.GetActiveScene().name);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                if(PlayerManager.LocalPlayerInstance == null)
+                {
+                    Debug.Log("We are Instantiating LocalPlayer from " + Application.loadedLevelName);
+                    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                } else
+                {
+                    Debug.Log("Ignoring scene load for " + SceneManager.GetActiveScene().name);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+
+
+        #region Public Methods
+
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.isMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.Log("PhotonNetwork: Loading Level : " + PhotonNetwork.room.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.room.PlayerCount);
+
+        }
+        #endregion
+
+        #region Photon Messages
+
+        public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+        {
+            Debug.Log("OnPhotonPlayerConnected() " + newPlayer.NickName);
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPHotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+                LoadArena();
+            }
+        }
+
+        public override void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer)
+        {
+            Debug.Log("OnPhotonPlayerDisconnected() " + otherPlayer.NickName);
+
+            if ( PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerDisconnected isMaster: " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+                LoadArena();
+            }
+        }
+
+
+        #endregion 
+    }
+}
---
+++ Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class PlayerNameInputField : MonoBehaviour {
+
+    #region private variables 
+
+    // store the playerpref Key to avoid typos
+    static string playerNamePrefKey = "PlayerName";
+
+    #endregion
+
+    #region MonBehaviour Callbacks
+
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity during initialization phase
+    /// </summary>
+    void Start () {
+        string defaultName = "";
+        InputField _inputField = this.GetComponent<InputField>();
+        if (_inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.playerName = defaultName;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void SetPlayerName(string value)
+    {
+        // #Important
+        PhotonNetwork.playerName = value + " "; // force a trailing space string in case value is an empty string, else playerName would not be updated.
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+    #endregion
+}
